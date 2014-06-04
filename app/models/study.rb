0ec560b8272592692d8c2674f46f1cc7c24d840d
Anupam Basu <nupebasu@gmail.com>
2014-06-03 20:11:33 -0500

    Nav Bar Additions
    Added links in the Nav bar to Categories and Location

--- app/models/study.rb
+++ app/models/study.rb
@@ -1,4 +1,9 @@
 class Study < ActiveRecord::Base
+
+  validates :diagnosis, :presence => true
+  validates :suject, :presence => true
+  validates :accession, :uniqueness => true
+
   belongs_to :user
   belongs_to :category
   belongs_to :body_parts
